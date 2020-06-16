be921a0fe1b051cb4d4153ced4cae1508e0b898e
Ronald <ronystivens@hotmail.com>
2020-06-16 12:26:18 +0200

    estableciendo estrructura del controlador y del modelo

---
+++ controllers/movie-controller.js
@@ -0,0 +1,34 @@
+const movieModel = require('../models/movie-model');
+
+const ControllerMovie = ()=>{
+};
+
+ControllerMovie.error404 = (req, res, next)=>{
+
+};
+
+ControllerMovie.getAll = (req, res, next)=>{
+
+};
+
+ControllerMovie.add = (req, res, next)=>{
+
+};
+
+ControllerMovie.saveAdd = (req, res, next)=>{
+
+};
+
+ControllerMovie.update = (req, res, next)=>{
+
+};
+
+ControllerMovie.saveUpdate = (req, res, next)=>{
+
+};
+
+ControllerMovie.delete = (req, res, next)=>{
+
+};
+
+module.exports = ControllerMovie;
---
+++ models/movie-model.js
@@ -0,0 +1,29 @@
+const movieModel = require('./movie-schema');
+
+
+const Movie = ()=>{
+};
+
+Movie.getAll = ()=>{
+
+};
+
+Movie.save = ()=>{
+
+};
+
+
+Movie.update = ()=>{
+
+};
+
+Movie.saveUpdate = ()=>{
+
+};
+
+Movie.delete = ()=>{
+
+};
+
+
+module.exports = Movie;
