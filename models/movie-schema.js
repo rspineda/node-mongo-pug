4fcf3e0668d0797ffa9c3226080d39702d1d0501
Ronald <ronystivens@hotmail.com>
2020-06-16 22:10:45 +0200

    conexión con Mongo y esquema de Mongoose

---
+++ models/movie-schema.js
@@ -0,0 +1,34 @@
+const config = require('./db-config'),
+        mongoose = require('mongoose'),
+        Schema = mongoose.Schema;
+
+       
+//mongoose.connect(`mongodb://${config.mongo.host}/${config.mongo.db}`, );
+
+
+//estructura del esquema de las pelis en mongo
+const movieSchema = new Schema({
+    movie_id : "string",
+    title : "string",
+    release_year: "string",
+    rating : "string",
+    image : "string"
+});
+
+//creo el modelo para utilizar el esquema de Mongoose
+Movie = mongoose.model ("Movie", movieSchema);
+
+
+//conecto con mongo 
+const connect = () => {
+    return  mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+ }
+ 
+ connect()
+     .then(data => console.log("CONNECTED--->", data))
+     .catch(err => console.warn("ERROR!--->", err));
+
+
+module.exports = Movie;
+
+
